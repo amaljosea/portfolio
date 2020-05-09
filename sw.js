353e904a2e42d2396b5b2b634affa9df8fe4dd57
Amal Jose <amal@dexlock.com>
2020-05-09 23:18:42 +0530

    Add cache

--- sw.js
+++ sw.js
@@ -1,9 +1,25 @@
+const CACHE_NAME = "V1";
+
 self.addEventListener("install", (event) => {
   console.log("Service Worker installing.");
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activating.");
+  // delete any unexpected caches
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => keys.filter((key) => key !== CACHE_NAME))
+      .then((keys) =>
+        Promise.all(
+          keys.map((key) => {
+            console.log(`Deleting cache ${key}`);
+            return caches.delete(key);
+          })
+        )
+      )
+  );
 });
 
 function cache(request, response) {
@@ -16,17 +32,16 @@ function cache(request, response) {
     .then((cache) => cache.put(request, response.clone()));
 }
 
-
-self.addEventListener("fetch", event => {
-    // Cache-First Strategy
-    event.respondWith(
-      caches
-        .match(event.request) // check if the request has already been cached
-        .then(cached => cached || fetch(event.request)) // otherwise request network
-        .then(
-          response =>
-            cache(event.request, response) // put response in cache
-              .then(() => response) // resolve promise with the network response
-        )
-    );
-  });
+self.addEventListener("fetch", (event) => {
+  // Cache-First Strategy
+  event.respondWith(
+    caches
+      .match(event.request) // check if the request has already been cached
+      .then((cached) => cached || fetch(event.request)) // otherwise request network
+      .then(
+        (response) =>
+          cache(event.request, response) // put response in cache
+            .then(() => response) // resolve promise with the network response
+      )
+  );
+});
